d8dd259752185c1e436d8c8402a995d0df92369f
AgusGunawan96 <mragusgunawan78@gmail.com>
2021-08-31 09:15:18 +0700

    ecommerce

---
+++ application/config/recaptcha.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+// To use reCAPTCHA, you need to sign up for an API key pair for your site.
+// link: http://www.google.com/recaptcha/admin
+$config['recaptcha_site_key'] = '6LfdJoEaAAAAAGoX5_T3UdHR1lEAnYpECHoYllR4';
+$config['recaptcha_secret_key'] = '6LfdJoEaAAAAALi-1oX6J8BYwPPMLV2rBa_tK78S';
+
+// reCAPTCHA supported 40+ languages listed here:
+// https://developers.google.com/recaptcha/docs/language
+$config['recaptcha_lang'] = 'id';
+
+/* End of file recaptcha.php */
+/* Location: ./application/config/recaptcha.php */
